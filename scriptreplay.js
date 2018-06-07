94803aec4ca9c0760da3dd7d7c3ec53a8420a84e
rich <h43z@users.noreply.github.com>
2018-06-07 20:20:26 +0200

    dependecies are now loaded by scriptreplay

--- scriptreplay.js
+++ scriptreplay.js
@@ -1,45 +1,56 @@
-async function scriptreplay(typescriptFile, timingFile){
+function loadScript(url, callback){
+  let script = document.createElement('script')
+  script.onload = callback
+  script.src = url
+  document.head.appendChild(script)
+}
 
+function loadCss(url, callback){
+  let link = document.createElement('link')
+  link.href =  url
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.onload = callback
+  document.head.appendChild(link)
+}
+
+async function scriptreplay(typescriptFile, timingFile, element){
   let typescript
   let timings = []
 
   const term = new Terminal()
-  term.open(document.getElementById('terminal'))
+  term.open(element, false)
 
   await fetch(typescriptFile)
     .then(response => response.text())
     .then(text  => typescript = text)
 
-  //remove first line
   typescript = typescript.replace(/.*\n/, '')
 
   await fetch(timingFile)
     .then(response => response.text())
-    .then(text  => {
-      timings = text.split('\n') 
-    })
-
+    .then(text  => timings = text.split('\n'))
 
   function print(line, lastPos, output){
     let [time, length] = timings[line].split(' ')
 
-    console.log(line, time, length)
-
     if(!length || !time){
+      term.clear()
+      print(0, 0, '')
       return
     }
 
     term.write(output)
-
     output = typescript.slice(lastPos, lastPos + parseInt(length))
     lastPos += parseInt(length)
 
     setTimeout((ln, lp, o) => {
       print(ln, lp, o)
-    }, time*1000, ++line, lastPos, output)
+    }, parseFloat(time) * 1000, ++line, lastPos, output)
   }
-
 
   print(0, 0, '')
 }
 
+loadCss('https://cdnjs.cloudflare.com/ajax/libs/xterm/2.9.2/xterm.css')
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/xterm/2.9.2/xterm.js')
